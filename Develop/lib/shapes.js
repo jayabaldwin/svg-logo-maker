1553a4586304f8cc607ef8652086e71c157856b1
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-05 12:34:54 +1100

    shapes file

--- Develop/lib/shapes.js
+++ Develop/lib/shapes.js
@@ -1,25 +1,43 @@
 // Exports `Triangle`, `Circle`, and `Square` classes
 class Shape {
-    constructor (text, textColor, shape, fillColor) {
+    constructor(text, color, background) {
         this.text = text;
-        this.textColor = textColor;
-        this.shape = shape;
-        this.fillColor = fillColor;
+        this.color = color;
+        this.background = background;
     }
 }
 
+
 class Triangle extends Shape {
+    constructor(text, color, background) {
+        super(text, color, background);
+    }
     
+    render(text, color, background) {
+        return `<polygon points="150, 18 244, 182 56, 182" fill='${background}'></polygon><text y="150" x="150" alignment-baseline="center" fill="${color}" text-anchor="middle" font-size="30">${text}</text>`
+    } 
 }
+
 
 class Circle extends Shape {
+    constructor(text, color, background) {
+        super(text, color, background);
+    }
 
+    render(text, color, background) {
+        return `<circle cx="150" cy="150" r="50" fill='${background}'></circle><text y="150" x="150" alignment-baseline="center" fill="${color}" text-anchor="middle" font-size="30">${text}</text>`
+    }
 }
+
 
 class Square extends Shape {
+    constructor(text, color, background) {
+        super(text, color, background);
+    }
 
+    render(text, color, background) {
+        return `<rect x="0" y="0" height="200" width="300" fill="${background}"></rect><text y="100" x="150" alignment-baseline="center" fill="${color}" text-anchor="middle" font-size="30">${text}</text>`
+    }
 }
-// Use inheritance to reuse the code in the child classes.
-// Must have max 3 characters
-// Exports shapes
+
 module.exports = { Triangle, Circle, Square };
--- Develop/lib/shapes.test.js
+++ Develop/lib/shapes.test.js
@@ -5,26 +5,28 @@ const { Triangle, Square, Circle } = require("../lib/shapes.js");
 describe('Triangle', () => {
   test("", () => {
     const shape = new Triangle();
-    shape.setColor("blue");
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
+    shape.setColor("purple");
+    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="purple" />'
     );
   });
 });
+
 
 // Unit testing: circle 
 describe('Circle', () => {
   test("", () => {
-    const shape = new Triangle();
-    shape.setColor("blue");
-    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
+    const shape = new Circle();
+    shape.setColor('#2A80B9');
+    expect(shape.render()).toEqual('<circle cx="150" cy="150" r="50" fill="#2A80B9" />'    
     );
   });
 });
 
+
 // Unit testing: square 
 describe('Square', () => {
   test("", () => {
-    const shape = new Triangle();
+    const shape = new Square();
     shape.setColor("blue");
     expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
     );
