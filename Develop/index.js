2dabb3250ea31304a2e92a7c3d8276a86fa678b6
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2024-02-05 11:37:55 +1100

    user input and write to file documents updated and simplified to be utilised by index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,97 +1,22 @@
-// Runs the application using imports from lib/
-
 // Packages needed for this application
 const inquirer = require('inquirer');
 const jest = require('jest')
 const fs = require('fs');
-const shapes = require('./lib/shapes.js');
-const userInput = require('./lib/user-input.js');
-const writeFile = require('./lib/write-file.js');
-
-// Packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generatedmarkdown.js')
-
-// Inquirer prompts
-const questions = [
-    {
-        type: 'input',
-        name: 'github',
-        message: "What is your GitHub username?",
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is your email address?",
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: "What is the title of your project?",
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: "Please write a short description of your project.",
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: "Select a license for your application from the following list",
-        choices: ['MIT', 'APACHE 2.0', 'Boost Software 1.0', 'BSD 3', 'None'],
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: "What command should be run to install dependencies?",
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: "What command should be run to run tests?",
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: "What does the user need to know about this repository?",
-    },
-    {
-        type: 'list',
-        name: 'contributionQuestion',
-        message: "Will you be accepting contributions to the repo?",
-        choices: ['I will', 'I will not']
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: "If you answered 'I will' to the previous question. Please specify any guidelines you have with contribution, if not, leave blank",
-    },
-    {
-        type: 'list',
-        name: 'questions',
-        message: "Where would you like users to reachout with questions regarding this application?",
-        choices: ['GitHub', 'Email']
-    }
-];
+const shapes = require('../lib/shapes.js');
+const userInput = require('../lib/user-input.js');
+const writeFile = require('../lib/write-file.js');
 
 
-// Write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        err ? console.log(err) : console.log('Successfully created README.md!')
-    });
-}
 
 // Initialize app
 function init() {
     // prompt inquirer questions
     inquirer.prompt(questions)
         .then((answers) => {
-            // pass inquirer answers into generate markdown 
+            // pass inquirer answers into write-file
             let markdownContent = generateMarkdown(answers);
             // pass markdown content to writeToFile
-            writeToFile('README.md', markdownContent);
+            writeToFile('logo.svg', markdownContent);
         })
         .catch((err) => console.error(err));
 }
--- Develop/lib/user-input.js
+++ Develop/lib/user-input.js
@@ -1,8 +1,4 @@
 const inquirer = require('inquirer');
-const Triangle = require('./triangle.js');
-const Circle = require('./circle.js');
-const Square = require('./square.js');
-const fs = require('fs');
 
 // Inquirer prompts
 const questions = [
@@ -10,6 +6,12 @@ const questions = [
         type: 'input',
         name: 'text',
         message: 'Enter the text you would like to include on your logo (Max: 3 characters)',
+        validate: function (input) {
+            if (input.length > 3 || input.length <=0){
+                return "Please enter between 1-3 characters for your logo text";
+            }
+            return true;
+        }
     },
     {
         type: 'input',
@@ -28,5 +30,3 @@ const questions = [
         message: 'Enter background color (Color keywork or hexidecimal code)?',
     }
 ];
-
-module.exports = Questions;
